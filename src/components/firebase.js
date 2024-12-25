6366a4e27b6f14210200e1057c582ac3333149e5
unknown <ashunamdev7777@gmail.com>
2024-12-25 23:27:52 +0530

    Last commit

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,23 +4,13 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDrB9FupDgjjVk4_guLC24ydG3retE3bME",
-//   authDomain: "login-auth-4736e.firebaseapp.com",
-//   projectId: "login-auth-4736e",
-//   storageBucket: "login-auth-4736e.appspot.com",
-//   messagingSenderId: "10562914305",
-//   appId: "1:10562914305:web:2cff37be4fa9ccf0a29800"
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDYp_MYOpBgfRCgx2pwbx-yyyAU54RHPUY",
-  authDomain: "signuplogin-form.firebaseapp.com",
-  projectId: "signuplogin-form",
-  storageBucket: "signuplogin-form.firebasestorage.app",
-  messagingSenderId: "54342412057",
-  appId: "1:54342412057:web:598b603edf6b7bbd808788"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
